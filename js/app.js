24d84f37a9c87ae4c13edb64f4c17956edb84444
Varga Zsolt <shadowvzs@hotmail.com>
2022-08-28 18:42:50 +0300

    just clear interval when countdown reach the 0

--- js/app.js
+++ js/app.js
@@ -24,12 +24,13 @@
         const now = Math.floor(Date.now() / 1000);
         const target = Math.floor(new Date(new Date().toISOString().substring(0, 10) + ' ' + elements.startAtInput.value + ':00').getTime() / 1000);
         if (diff < 0) { return alert('Target is in past'); }
-        diff = target - now;
+        diff = target - now; s
         elements.text.textContent = elements.textInput.value;
         updateCountdownText(diff + 1);
 
         const intervalId = window.setInterval(() => {
             updateCountdownText(diff);
+            if (diff === 0) { clearInterval(intervalId); }
             diff--;
         }, 1000)
     };
